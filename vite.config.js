dd039f664ae3006efa44e6ceff5f31e2383df782
Abdullah Mahmoud <abdullah@example.com>
2025-09-06 18:38:56 +0300

    save local changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/job-board/', // اسم الريبو على GitHub
+  base: '/job-board/',  // اسم الريبو بتاعك
   plugins: [react()],
 })
